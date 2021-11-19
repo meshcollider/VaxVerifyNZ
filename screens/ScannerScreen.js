32658a1d0ea444ca6e8756501c5583a57244acab
Samuel Dobson <dobsonsa68@gmail.com>
2021-11-19 13:41:02 +1300

    Add pop up menu

--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -3,6 +3,7 @@ import { View, Vibration, SafeAreaView, Text, TouchableOpacity } from 'react-nat
 import { useIsFocused } from '@react-navigation/native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Menu, MenuItem, MenuDivider } from 'react-native-material-menu';
 
 import ScanCamera from '../components/Camera.js'
 import Styles from '../config/styles.js'
@@ -10,6 +11,7 @@ import Styles from '../config/styles.js'
 export default function ScannerScreen({ navigation }) {
     const isFocused = useIsFocused()
     const [shouldVibrate, setVibrate] = React.useState(true)
+    const [menuVisible, setMenuVisible] = React.useState(false)
 
     React.useEffect(() => {
         ;(async () => {
@@ -23,6 +25,10 @@ export default function ScannerScreen({ navigation }) {
         await AsyncStorage.setItem('@vaxverifynz-vibrate', JSON.stringify(shouldVibrate))
     }
 
+
+    const hideMenu = () => setMenuVisible(false);
+    const showMenu = () => setMenuVisible(true);
+
     const handleBarCodeScanned = ({ type, data }) => {
         if (shouldVibrate) {
             Vibration.vibrate(200)
@@ -30,14 +36,28 @@ export default function ScannerScreen({ navigation }) {
         navigation.navigate('ScanResult', { data: data })
     }
 
+    const showAbout = () => {
+        navigation.navigate('About')
+    }
+
     return (
         <View style={Styles.container}>
             <SafeAreaView style={Styles.settingsBox}>
-                <TouchableOpacity style={Styles.settingsButton} onPress={() => toggleVibrate()}>
-                    <Text style={Styles.buttonLabel}>
-                        <MaterialCommunityIcons size={30} name="dots-vertical" />
-                    </Text>
-                </TouchableOpacity>
+                <View style={Styles.settingsButton}>
+                <Menu
+                    visible={menuVisible}
+                    anchor={
+                            <Text onPress={showMenu} style={Styles.buttonLabel}>
+                                <MaterialCommunityIcons size={30} name="dots-vertical" />
+                            </Text>
+                    }
+                    onRequestClose={hideMenu}
+                >
+                    <MenuItem onPress={toggleVibrate}>Turn {shouldVibrate ? 'off' : 'on'} vibration</MenuItem>
+                    <MenuDivider />
+                    <MenuItem onPress={showAbout}>About</MenuItem>
+                </Menu>
+                </View>
             </SafeAreaView>
 
             {isFocused && <ScanCamera resultHandler={handleBarCodeScanned} />}
